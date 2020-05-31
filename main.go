55e22af9fceb050ec44250a743f131aa00964e78
jsonLiang <yuanyu90221@gmail.com>
2020-05-31 22:43:19 +0800

    job queue

--- main.go
+++ main.go
@@ -1,25 +1,127 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
-func main() {
-	ch := make(chan int, 2)
+// Consumer struct
+type Consumer struct {
+	inputChan chan int
+	jobChan   chan int
+}
+
+func getRandomTime() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(10)
+}
+
+func withContextFunc(ctx context.Context, f func()) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
 	go func() {
-		ch <- 1
-		ch <- 2
-		// close(ch)
-	}()
-	// for n := range ch { // 如果沒有close channel 這邊就會一直read
-	// 	fmt.Println(n)
-	// }
-	go func() {
-		for n := range ch {
-			fmt.Println(n)
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		defer signal.Stop(c)
+
+		select {
+		case <-ctx.Done():
+		case <-c:
+			cancel()
+			f()
 		}
 	}()
 
-	time.Sleep(1 * time.Second)
+	return ctx
+}
+
+func (c *Consumer) queue(input int) bool {
+	select {
+	case c.inputChan <- input:
+		log.Println("already send input value:", input)
+		return true
+	default:
+		return false
+	}
+}
+
+func (c Consumer) startConsumer(ctx context.Context) {
+	for {
+		select {
+		case job := <-c.inputChan:
+			if ctx.Err() != nil {
+				close(c.jobChan)
+				return
+			}
+			c.jobChan <- job
+		case <-ctx.Done():
+			close(c.jobChan)
+			return
+		}
+	}
+}
+
+func (c *Consumer) process(num, job int) {
+	n := getRandomTime()
+	log.Printf("Sleeping %d secondes...\n", n)
+	time.Sleep(time.Duration(n) * time.Second)
+	log.Println("worker:", num, " job value:", job)
+}
+
+func (c *Consumer) worker(ctx context.Context, num int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println("start the worker", num)
+	for {
+		select {
+		case job := <-c.jobChan:
+			if ctx.Err() != nil {
+				log.Println("get next job", job, "and close the worker", num)
+				return
+			}
+			c.process(num, job)
+		case <-ctx.Done():
+			log.Println("close the worker", num)
+			return
+		}
+	}
+}
+
+const poolSize = 2
+
+func main() {
+	finished := make(chan bool)
+	wg := &sync.WaitGroup{}
+	wg.Add(poolSize)
+	// create the consumer
+	consumer := Consumer{
+		inputChan: make(chan int, 10),
+		jobChan:   make(chan int, poolSize),
+	}
+
+	ctx := withContextFunc(context.Background(), func() {
+		log.Println("cancel from ctrl+c event")
+		wg.Wait()
+		close(finished)
+	})
+
+	for i := 0; i < poolSize; i++ {
+		go consumer.worker(ctx, i, wg)
+	}
+
+	go consumer.startConsumer(ctx)
+	go func() {
+		consumer.queue(1)
+		consumer.queue(2)
+		consumer.queue(3)
+		consumer.queue(4)
+		consumer.queue(5)
+	}()
+
+	<-finished
+	log.Println("Game over")
 }
